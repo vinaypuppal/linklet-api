9088c737acdfb3558c6e50fb7b1ac4d7c398bc3c
Vinay Puppal <me@vinaypuppal.com>
2017-04-16 00:45:17 +0530

    fix typo

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -1,4 +1,4 @@
-const { User } = require('../models/user')
+const User = require('../models/user')
 const querystring = require('qs')
 
 const redirectWithQueryString = (res, data, appRedirectUrl) => {
